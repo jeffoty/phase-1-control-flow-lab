cb1831a7ccd1fd27ddf9e885ef5a4026090d6301
Jeff <odhiambo47@gmail.com>
2022-08-18 19:13:32 +0300

    completed assignment

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function scuberGreetingForFeet(ride){
   if (ride <= 400) {
     return "This one is on me!";
   }
-  else if (ride > 2000){
+  else if (ride > 2000 && ride < 2500){
     return "I will gladly take your thirty bucks.";
   }
   else {
